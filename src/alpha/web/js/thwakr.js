a785c3475bba748961e8af273c825ebacc742580
unknown <andrew@.(none)>
2011-04-17 01:35:48 -0700

    Starting checkin

---
+++ src/alpha/web/js/thwakr.js
@@ -0,0 +1,37 @@
+/**
+ * @author andrew
+ */
+
+// yui adds
+
+YUI.add("thwakr", function(Y){
+	var Lang = Y.Lang,
+	 	CBX  = 'contentBox', 
+        BBX  = 'boundingBox',
+		NAME = 'thwakr';
+	
+	Y.Thwakr = Y.Base.create(
+		NAME,
+		Y.Widget,
+		Y.WidgetParent,
+		{
+			//Instance members here
+		},{
+			//Static members here, specially:
+			ATTRS: {
+				
+			}
+		}
+	);
+	
+	 
+},'0.01',{
+	requires: ['widget','widget-parent']
+})
+
+
+//main
+
+YUI().use("node", "gallery-storage-lite", function(Y){
+	Y.log("in startup...");
+});
