381c34d2ddc4bbb918c6e961ad1f3590419f73c8
Andrew Wooldridge <triptych@whisperstorm.com>
2011-08-24 20:38:28 +0000

    coding

--- src/yui_mvc/web/js/thwakr.js
+++ src/yui_mvc/web/js/thwakr.js
@@ -1,17 +1,31 @@
-YUI().use("node", "event","tabview", function(Y){
-    
-    Y.on("domready", function(){
+YUI().use("node", "event", "tabview", function(Y) {
+
+    Y.on("domready", function() {
         Y.log("YUI loaded...");
-        
-            var tabview = new Y.TabView({
-        srcNode: '#demo'
-    });
 
-    tabview.render();
-        
+        var tabview = new Y.TabView({
+            srcNode: '#demo'
         });
-        
-        
-        
 
+        tabview.render();
+
+/**
+var opts = {
+  lines: 12, // The number of lines to draw
+  length: 7, // The length of each line
+  width: 5, // The line thickness
+  radius: 10, // The radius of the inner circle
+  color: '#000', // #rbg or #rrggbb
+  speed: 1, // Rounds per second
+  trail: 100, // Afterglow percentage
+  shadow: true // Whether to render a shadow
+};
+var target = document.getElementById('foo');
+var spinner = new Spinner(opts).spin(target);
+**/
     });
+
+
+
+
+});
